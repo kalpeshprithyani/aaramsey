8775ebda5f2d1599ec588e7afe9d9aeed6cc1339
Kalpesh Prithyani <kalpeshprithyani@gmail.com>
2020-02-12 00:38:09 +0530

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.carousel.carousel-slider').carousel({
+        fullWidth: true
+    });
+    $('.dropdown-trigger').dropdown();
+    $('.modal').modal();
+    $('.collapsible').collapsible({accordion: false});
+});

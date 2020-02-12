6aa1e4e8e3c87360e97b0902b0e9e837d6f482c8
Kalpesh Prithyani <kalpeshprithyani@gmail.com>
2020-02-12 16:45:42 +0530

    responsive

--- js/custom.js
+++ js/custom.js
@@ -5,4 +5,17 @@ $(document).ready(function(){
     $('.dropdown-trigger').dropdown();
     $('.modal').modal();
     $('.collapsible').collapsible({accordion: false});
+    $('.datepicker').datepicker({
+        format: 'dd mmm',
+        defaultDate: new Date(),
+        setDefaultDate: true
+    });
+    $('.sidenav').sidenav();
+    $('.timepicker').timepicker();
+
+    $('.with-gap[checked]').parent().parent().addClass('active');
+    $(document).on('click', '.address-block', function(e) {
+        const $el = $(this);
+        $el.addClass("active").siblings().removeClass('active');
+    });
 });

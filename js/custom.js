922d986c40272e5398dc69228d33a626c1b5e47b
Kalpesh Prithyani <kalpeshprithyani@gmail.com>
2020-02-12 17:48:35 +0530

    responsive 3

--- js/custom.js
+++ js/custom.js
@@ -19,10 +19,4 @@ $(document).ready(function(){
         $el.addClass("active").siblings().removeClass('active');
     });
 
-    setTimeout(function () {
-        let viewheight = $(window).height();
-        let viewwidth = $(window).width();
-        let viewport = document.querySelector("meta[name=viewport]");
-        viewport.setAttribute("content", "height=" + viewheight + "px, width=" + viewwidth + "px, initial-scale=1.0");
-    }, 300);
 });
